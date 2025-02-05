3243e9b3d7e3712eda5c3311813650d451bd4564
Gisel Garrido <ggarr55@wgu.edu>
2025-02-05 03:03:29 -0700

    add about page body

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")  # This will load an HTML page
 
+@app.route('/about')
+def about():
+    return render_template('about.html')  # Will render about page
+
 if __name__ == "__main__":
     app.run(debug=True)
