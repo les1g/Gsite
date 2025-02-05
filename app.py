af8b713dadca94e670f06608d45f952d47d6bb84
Gisel Garrido <ggarr55@wgu.edu>
2025-02-05 09:06:23 -0700

    Create base page

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")  # This will load an HTML page
+    return render_template('index.html')  # Home page
 
 @app.route('/about')
 def about():
-    return render_template('about.html')  # Will render about page
+    return render_template('about.html')  # About Me page
 
-if __name__ == "__main__":
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')  # Portfolio page
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')  # Resume page
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  # Contact page
+
+if __name__ == '__main__':
     app.run(debug=True)
